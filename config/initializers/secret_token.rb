252f0c4ff751694ec5d9c0389e4198c14412bc42
daifu <vagrant@rails-dev-box>
2013-03-30 07:22:30 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TubeMusic::Application.config.secret_token = '0e838ba3ff9bd052525d0a3fe15c7459c8d055b444b756c111ce69b98dcc7c6b9b0321dda3e35a1c77d60e48738a983b8a34b7ac6cd86d8509ea7535cb30ce3b'
